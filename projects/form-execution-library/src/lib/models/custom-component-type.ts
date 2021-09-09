66e912af5156451db49d275088eb13981cd596ca
Michael Pierce <mpierce@mastercontrol.com>
2021-09-09 10:12:27 -0600

    re-added ControlValueAccessor as component extension requirement

--- projects/form-execution-library/src/lib/models/custom-component-type.ts
+++ projects/form-execution-library/src/lib/models/custom-component-type.ts
@@ -1,5 +1,10 @@
 import { ComponentData } from './component-data';
+import { ControlValueAccessor } from '@angular/forms';
 
-export interface CustomComponentType {
+/*
+  This is where you can create a contract about functionality a component must have in order to register with your
+  domain registry. ex: undo/redo, ControlValueAccessor, etc.
+ */
+export interface CustomComponentType extends ControlValueAccessor {
   data: ComponentData;
 }
